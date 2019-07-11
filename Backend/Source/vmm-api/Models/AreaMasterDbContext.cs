6c41b7ac241b5b1fb1aff584a4c8eb04923cd539
Jitendra Prajapati <prajapatijv@hotmail.com>
2019-07-11 21:37:49 +1000

    First commit.

---
+++ Backend/Source/vmm-api/Models/AreaMasterDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vmm_api.Models
+{
+    [Table("Area_Master")]
+    public class AreaMaster
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Area_Id")]
+        public int Id { get; set; }
+
+        [Column("Area_Name", TypeName = "nvarchar(100)")]
+        public string AreaName { get; set; }
+    }
+
+    public class AreaMasterContext : DbContext
+    {
+        public AreaMasterContext(DbContextOptions<AreaMasterContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AreaMaster> AreaMasters { get; set; }
+    }
+}
