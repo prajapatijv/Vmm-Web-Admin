e81866c19d6f912d20e972dca537ddd539a10711
Jitendra.Prajapati <Jitendra.Prajapati@3PAUS160>
2019-07-29 13:13:36 +1000

    Remove Hash-Source.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,6 @@
 import React from 'react'
-import { createHistory, LocationProvider } from '@reach/router'
-import createHashSource from 'hash-source'
-
 import Routes from './routes'
 
-
-let source = createHashSource()
-let history = createHistory(source)
-
-const App = (props) => 
-<LocationProvider history={history}>
-    <Routes {...props} />
-</LocationProvider>
+const App = (props) => <Routes {...props} />
 
 export default App
