222095c36e4a2370cdeed679f7fba1c5e46bf6d0
prajapatijv@gmail.com <prajapatijv@gmail.com>
2019-08-13 20:57:18 +1000

    Added Base class.

---
+++ Backend/Source/VmmApi.Net.DataServices/Repository/IRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VmmApi.Net.DataServices.Entities;
+
+namespace VmmApi.Net.DataServices.Repository
+{
+    public class BaseRepository<T> where T : IEntity<T> 
+    {
+        private readonly DbContext dbContext;
+        public BaseRepository(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Save(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
