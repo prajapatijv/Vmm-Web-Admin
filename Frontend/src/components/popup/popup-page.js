1cdf09e1f892a231b3082c4733e8bf4d8ba550a7
Jitendra.Prajapati <Jitendra.Prajapati@3PAUS160>
2019-08-21 11:59:50 +1000

    Updated popup list component

--- Frontend/src/components/popup/popup-page.js
+++ Frontend/src/components/popup/popup-page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import PopupForm from './popup-form'
+import NavLink from '../shared/navlink'
 import PageWrapper from '../shared/page-wrapper'
 
 const PopupPage = (props) => {
@@ -12,6 +13,14 @@ const PopupPage = (props) => {
     )
 }
 
-const PopupLines = () => <div></div>
+const PopupLines = ({ popups }) =>
+    popups.map((popup) =>
+        <NavLink key={popup.id}
+            to={`/popup/${popup.id}`}
+            activeClassName="active"
+            className="app-list-item list-group-item-action"
+        >{popup.description} 
+        </NavLink>
+    )
 
 export default PopupPage
