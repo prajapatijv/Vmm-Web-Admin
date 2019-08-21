5e4501742267b933fa2125e6f99253f5d79f8cda
Jitendra.Prajapati <Jitendra.Prajapati@3PAUS160>
2019-08-21 11:55:55 +1000

    Rollback changes.

--- Frontend/src/containers/popup-page/reducer.js
+++ Frontend/src/containers/popup-page/reducer.js
@@ -3,6 +3,16 @@ import withPageReducer from '../../utility/with-page-reducer'
 
 const popups = (state=initialState, action) => {
     return withPageReducer(keys.Popup, state, action, defaultPopup, (criteria) => byName(criteria))
+
+    /*switch (action.type) {
+        case `FETCH_POPUPS_SUCCEED`: {
+            return {...popupState, "popup": action.payload.data.popup[0] }
+        }
+
+        default:
+            return popupState
+    }*/
+
 }
 
 const defaultPopup = { 
