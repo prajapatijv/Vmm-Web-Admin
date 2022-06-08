a043e037a52754ba0326b165508ba1382f45ae3c
Jitendra Prajapati <z1211241@wowcorp.com.au>
2022-06-09 09:39:32 +1000

    Enable allowdownload

--- Frontend/src/AppConfig.js
+++ Frontend/src/AppConfig.js
@@ -4,14 +4,14 @@ export const Env = {
 }
 
 const _contextMappings = {
-    'user' : { 'apiContext': 'users', 'actionContext': { 'singular': 'user', 'SINGULAR': 'USER', 'plural': 'users', 'PLURAL': 'USERS' } },
-    'item' : { 'apiContext': 'items', 'actionContext': { 'singular': 'item', 'SINGULAR': 'ITEM', 'plural': 'items', 'PLURAL': 'ITEMS' } },
-    'documenttype' : { 'apiContext': 'documenttypes', 'actionContext': { 'singular': 'documenttype', 'SINGULAR': 'DOCUMENTTYPE', 'plural': 'documenttypes', 'PLURAL': 'DOCUMENTTYPES' } },
-    'event' : { 'apiContext': 'events', 'actionContext': { 'singular': 'event', 'SINGULAR': 'EVENT', 'plural': 'events', 'PLURAL': 'EVENTS' } },
-    'eventtype' : { 'apiContext': 'eventtypes', 'actionContext': { 'singular': 'eventtype', 'SINGULAR': 'EVENTTYPE', 'plural': 'eventtypes', 'PLURAL': 'EVENTTYPES' } },
-    'area' : { 'apiContext': 'areas', 'actionContext': { 'singular': 'area', 'SINGULAR': 'AREA', 'plural': 'areas', 'PLURAL': 'AREAS' } },    
+    'user' : { 'apiContext': 'users', 'actionContext': { 'singular': 'user', 'SINGULAR': 'USER', 'plural': 'users', 'PLURAL': 'USERS', 'allowDownload':0 } },
+    'item' : { 'apiContext': 'items', 'actionContext': { 'singular': 'item', 'SINGULAR': 'ITEM', 'plural': 'items', 'PLURAL': 'ITEMS','allowDownload':1 } },
+    'documenttype' : { 'apiContext': 'documenttypes', 'actionContext': { 'singular': 'documenttype', 'SINGULAR': 'DOCUMENTTYPE', 'plural': 'documenttypes', 'PLURAL': 'DOCUMENTTYPES','allowDownload':0 } },
+    'event' : { 'apiContext': 'events', 'actionContext': { 'singular': 'event', 'SINGULAR': 'EVENT', 'plural': 'events', 'PLURAL': 'EVENTS','allowDownload':1 } },
+    'eventtype' : { 'apiContext': 'eventtypes', 'actionContext': { 'singular': 'eventtype', 'SINGULAR': 'EVENTTYPE', 'plural': 'eventtypes', 'PLURAL': 'EVENTTYPES', 'allowDownload':0 } },
+    'area' : { 'apiContext': 'areas', 'actionContext': { 'singular': 'area', 'SINGULAR': 'AREA', 'plural': 'areas', 'PLURAL': 'AREAS', 'allowDownload':1 } },    
     'document' : { 'apiContext': 'documents', 'actionContext': { 'singular': 'document', 'SINGULAR': 'DOCUMENT', 'plural': 'documents', 'PLURAL': 'DOCUMENTS', 'allowDownload':1 } },    
-    'popup' : { 'apiContext': 'popups', 'actionContext': { 'singular': 'popup', 'SINGULAR': 'POPUP', 'plural': 'popups', 'PLURAL': 'POPUPS' , 'allowAdd':0, 'allowDelete':0 } },    
+    'popup' : { 'apiContext': 'popups', 'actionContext': { 'singular': 'popup', 'SINGULAR': 'POPUP', 'plural': 'popups', 'PLURAL': 'POPUPS' , 'allowAdd':0, 'allowDelete':0 , 'allowDownload':0} },    
     'query' : { 'apiContext': 'queries', 'actionContext': { 'singular': 'query', 'SINGULAR': 'QUERY', 'plural': 'queries', 'PLURAL': 'QUERIES', 'allowDownload':1 } },    
 }
 
